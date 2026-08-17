cb8d7069c17ee98bd318ce28030b4504d98ea249
Lucas Hussey <john.luke.hussey@gmail.com>
2026-08-16 22:32:55 -0400

    remove transaction cast

--- apps/server/src/repos/utils.ts
+++ apps/server/src/repos/utils.ts
@@ -4,5 +4,5 @@ import type { DbExecutor } from "./types.js";
 export async function withTransaction<T>(
   worker: (tx: DbExecutor) => Promise<T>,
 ): Promise<T> {
-  return db.transaction(async (tx) => worker(tx as DbExecutor));
+  return db.transaction(async (tx) => worker(tx));
 }
